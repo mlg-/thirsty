1e01ab90ea55e2a0e819d5fb191c2fe6291982d4
Mayouran Mahendran <mayo19@live.ca>
2015-06-29 17:17:48 -0400

    Refine Validations and Added Bar Index View

--- spec/features/bars/user_views_all_bars_spec.rb
+++ spec/features/bars/user_views_all_bars_spec.rb
@@ -1,14 +1,21 @@
 require 'rails_helper'
 
 feature 'user can view a list of bars', %Q{
-	Acceptance Criteria
+  Acceptance Criteria
 [] User can see a list of all bars sorted most recently created
 [] User does not have to be logged in to see a list of bars
 } do 
 
-	scenario 'user visits home page' do
-		visit '/'
+  scenario 'user visits home page' do
+    bar1 = FactoryGirl.create(:bar)
+    bar2 = FactoryGirl.create(:bar2) 
+    bar3 = FactoryGirl.create(:bar3)
+    visit '/'
 
-		expect(page).to have_content('Boston Bars')
-	end
+    expect(page).to have_content('Boston Bars')
+    expect(page).to have_content(bar1.name)
+    expect(page).to have_content(bar2.name)
+    expect(page).to have_content(bar3.name)
+
+  end
 end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -7,4 +7,23 @@ FactoryGirl.define do
     password_confirmation 'password'
   end
 
+  factory :bar do
+    name "Beacon Hill Pub"
+    address "123 Harrison Ave"
+    city "Boston"
+    state "MA"
+    zip "02156"
+
+    factory :bar2 do
+      name "Drink"
+      address "1234 Quenchtown Lane"
+    end
+
+    factory :bar3 do
+      name "Mayo's Frozen Bar"
+      address "22 Popsicle Lane"
+    end
+
+  end
+
 end
