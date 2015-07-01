61f7a838634a6217f206c60fcc7d9362e01e71da
Christine Dempsey <cvdemp@gmail.com>
2015-07-01 15:42:52 -0400

    Fix hound edits

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -71,10 +71,10 @@ class BarsController < ApplicationController
 
   private
 
-    def require_login
+  def require_login
       unless user_signed_in?
       flash[:error] = "You must be signed in to do that"
       redirect_to new_user_session_path
       end
-    end
+  end
 end
