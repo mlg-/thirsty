6966dd03f6b7ee4c37c85a817abf0c66a24f8fd6
Christine Dempsey <cvdemp@gmail.com>
2015-07-01 17:08:56 -0400

    Working development seed file to aid styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,1 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# require 'factory_girl'
-#
-# FactoryGirl.create_list(:bar, 12, message: "Created 12 new bars")
-# FactoryGirl.create_list(:user, 4, message: "Created 4 new users")
-# FactoryGirl.create(:admin, message: "Created admin")
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb"))
---
+++ db/seeds/development.rb
@@ -0,0 +1,17 @@
+
+n = rand(1..2000)
+12.times do
+  Bar.create!(name: "Mayos Frozen Bar #{n}",
+              address: "234 That Street #{n}",
+              city: "Boston",
+              state: "Massachusetts",
+              zip: "02345",
+              description:"You won't believe it. You just won't",
+              url: "http://thatsite.com",
+              photo_url: "http://thatothersite.com",
+              seating_capacity: 4,
+              food: true,
+              outdoor_seating: false,
+              pet_friendly: true)
+  n += 1
+end
