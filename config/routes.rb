e8ed27f3fe775165c9cd2ac92823cb7f63097dd7
DavidVoight <David.Voight@gmail.com>
2015-07-07 17:19:28 -0400

    Admin test suite nearly complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'bars#index'
   devise_for :users
-  get 'user/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
-  get 'user/:id' => 'users#show', as: :user
+  get 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
+  get 'users/:id' => 'users#show', as: :user
 
   resources :bars do
     resources :reviews, only: [:new, :create, :edit, :update, :destroy]
