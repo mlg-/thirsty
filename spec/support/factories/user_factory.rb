bc7dd234ca5533941144d1c15186949a7213a736
Melissa Leigh Gore <melissaleighgore@gmail.com>
2015-07-09 11:32:24 -0400

    Resolve hound errors

--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -3,7 +3,7 @@ require_relative 'bar_factory.rb'
 
 FactoryGirl.define do
   factory :user do
-    sequence(:email) {|n| "user#{n}@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password 'password'
     password_confirmation 'password'
     admin false
